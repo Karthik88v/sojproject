14f418ad310da8f0ad9c972ebe4a77c9fbad54c3
Karthik Vasudevan <karthik88v@gmail.com>
2022-10-20 13:50:09 +0100

    Initial commit

---
+++ src/main/java/com/example/sojproject/SojprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sojproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SojprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SojprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sojproject/SojprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sojproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SojprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
